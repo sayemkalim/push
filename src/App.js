ddb8128c04c85b98ca97aa8fcefb64dba8e8aefc
Sayem Kalim <101326225+sayemkalim@users.noreply.github.com>
2024-08-03 01:16:04 +0530

    ADD push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import addNotification from 'react-push-notification';
+const App = () => {
+    const buttonClick = () => {
+        addNotification({
+            title: 'Hello Ji',
+            subtitle: 'This is a subtitle',
+            message: 'This is a very long message',
+            theme: 'darkblue',
+            native: true // when using native, your OS will handle theming.
+        });
+    };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div> <button onClick={buttonClick} className="button">
+    Hello App
+   </button></div>
+  )
 }
 
-export default App;
+export default Push
